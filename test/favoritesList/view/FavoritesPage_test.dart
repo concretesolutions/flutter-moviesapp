0299d29f0b9657a2db27b46fbe3326f785dae105
cs-jose-freitas <jose.freitas@concrete.com.br>
2021-03-18 19:21:15 -0300

    Adds unit tests for favorites list

--- test/favoritesList/view/FavoritesPage_test.dart
+++ test/favoritesList/view/FavoritesPage_test.dart
@@ -1,23 +1,42 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:moviesapp/favoritesList/view/FavoriteCard.dart';
 import 'package:moviesapp/favoritesList/view/FavoritesPage.dart';
 import 'package:flutter/material.dart';
 
+import '../../mocks/FavoriteStorageMock.dart';
+import '../../stubs/MovieStubs.dart';
 import '../../utils/MockImageLoader.dart';
 import '../../utils/utils.dart';
 
 void main() {
   FavoritesPage sut;
+  FavoriteStorageMock mock;
 
   setUp(() {
-    sut = FavoritesPage(MockImageLoader());
+    mock = FavoriteStorageMock();
+    sut = FavoritesPage(MockImageLoader(), mock);
   });
 
-  group("FavoritesPage", () {
+  group("FavoritesPage -", () {
     testWidgets("List state", (WidgetTester tester) async {
+      final movie = MovieStub.stub();
+      mock.moviesToReturn = [movie, movie, movie, movie];
+
       await tester.pumpWidget(makeTestable(sut));
       final list = find.byType(ListView);
+      final cards = find.byType(FavoriteCard);
 
       expect(list, findsOneWidget);
+      expect(cards, findsNWidgets(4));
+    });
+
+    testWidgets("Empty state", (WidgetTester tester) async {
+      mock.moviesToReturn = [];
+
+      await tester.pumpWidget(makeTestable(sut));
+      final emptyState = find.text("You don't have any favorite movies yet. You can favorite in the list.");
+
+      expect(emptyState, findsOneWidget);
     });
   });
 }
---
+++ test/mocks/FavoriteStorageMock.dart
@@ -0,0 +1,22 @@
+import 'package:moviesapp/moviesList/model/Movie.dart';
+import 'package:moviesapp/storage/FavoriteStorageProtocol.dart';
+
+import '../stubs/MovieStubs.dart';
+
+class FavoriteStorageMock implements FavoriteStorageProtocol {
+  List<Movie> moviesToReturn = [MovieStub.stub(), MovieStub.stub()];
+
+  @override
+  void favoriteMovie(Movie movie) {}
+
+  @override
+  List<Movie> getFavoritesMovies() {
+    return moviesToReturn;
+  }
+
+  @override
+  bool isFavoriteMovie(int id) {}
+
+  @override
+  void unfavoriteMovie(int id) {}
+}
