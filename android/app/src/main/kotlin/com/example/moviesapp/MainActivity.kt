1f8622e4960a812e24107fce12a852710e03b2b2
cs-gabriela-lima <gabriela.s.lima@concrete.com.br>
2021-04-22 16:41:02 -0300

    first interaction with native app

--- android/app/src/main/kotlin/com/example/moviesapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/moviesapp/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.example.moviesapp
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.flutter.moviesapp/moviesapp"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine){
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler{ methodCall, result ->
+            if (methodCall.method == "Print"){
+                result.success("From kotlin")
+            }
+        }
+
+    }
 }
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 import 'view/TabBar.dart';
 
+// void main() {
+//   runApp(MoviesApp());
+// }
+//
+// class MoviesApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoApp(
+//       theme: CupertinoThemeData(
+//         barBackgroundColor: CupertinoColors.systemYellow,
+//         primaryColor: CupertinoColors.black,
+//       ),
+//       home: TabBar(),
+//     );
+//   }
+// }
+
 void main() {
-  runApp(MoviesApp());
+  runApp(MyApp());
 }
 
-class MoviesApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      theme: CupertinoThemeData(
-        barBackgroundColor: CupertinoColors.systemYellow,
-        primaryColor: CupertinoColors.black,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      home: TabBar(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = MethodChannel("com.flutter.moviesapp/moviesapp");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: MaterialButton(
+            child: Text("click"),
+            onPressed: () {
+              Print();
+            },
+          ),
+        ));
+  }
+
+  void Print() async {
+    String value;
+    try {
+      value = await platform.invokeMethod("Print");
+    } catch (e) {
+      print(e);
+    }
+    print(value);
   }
 }
