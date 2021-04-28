ac1b1660950a28dab52208db0c4d4d6aa4b0e5c4
cs-gabriela-lima <gabriela.s.lima@concrete.com.br>
2021-04-28 15:50:20 -0300

    add communication with android

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
-import 'view/TabBar.dart';
+import 'package:moviesapp/view/TabBar.dart';
 
-// void main() {
-//   runApp(MoviesApp());
-// }
-//
-// class MoviesApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return CupertinoApp(
-//       theme: CupertinoThemeData(
-//         barBackgroundColor: CupertinoColors.systemYellow,
-//         primaryColor: CupertinoColors.black,
-//       ),
-//       home: TabBar(),
-//     );
-//   }
-// }
 
 void main() {
-  runApp(MyApp());
+  runApp(MoviesApp());
 }
 
-class MyApp extends StatelessWidget {
-
+class MoviesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return CupertinoApp(
+      theme: CupertinoThemeData(
+        barBackgroundColor: CupertinoColors.systemYellow,
+        primaryColor: CupertinoColors.black,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TabBar(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const platform = MethodChannel("com.flutter.moviesapp/moviesapp");
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: MaterialButton(
-            child: Text("click"),
-            onPressed: () {
-              Print();
-            },
-          ),
-        ));
-  }
-
-  void Print() async {
-    String value;
-    try {
-      value = await platform.invokeMethod("Print");
-    } catch (e) {
-      print(e);
-    }
-    print(value);
   }
 }
--- lib/movieDetail/view/MovieDetail.dart
+++ lib/movieDetail/view/MovieDetail.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:moviesapp/movieDetail/viewModel/MovieDetailViewModel.dart';
 
 class MovieDetail extends StatefulWidget {
@@ -22,97 +26,32 @@ class _MovieDetailState extends State<MovieDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      backgroundColor: Colors.white,
-      navigationBar: CupertinoNavigationBar(middle: const Text("Movie")),
-      child: _body(),
+    final String viewType = '<platform-view-type>';
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+
+    return PlatformViewLink(
+      viewType: viewType,
+      surfaceFactory:
+          (BuildContext context, PlatformViewController controller) {
+        return AndroidViewSurface(
+            controller: controller,
+            gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{
+            },
+            hitTestBehavior: PlatformViewHitTestBehavior.opaque
+        );
+      },
+      onCreatePlatformView: (PlatformViewCreationParams params) {
+        return PlatformViewsService.initSurfaceAndroidView(
+          id: params.id,
+          viewType: viewType,
+          layoutDirection: TextDirection.ltr,
+          creationParams: creationParams,
+          creationParamsCodec: StandardMessageCodec(),
+        )
+          ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+          ..create();
+      },
     );
-  }
 
-  Widget _body() {
-    return Container(padding: EdgeInsets.all(16), child: _detailsContent());
-  }
-
-  Widget _detailsContent() {
-    return ListView(children: [
-      _imageView(),
-      SizedBox(height: 16),
-      _titleFavoriteRow(),
-      _space(),
-      _releaseDate(),
-      SizedBox(height: 16),
-      _overview()
-    ]);
-  }
-
-  Widget _space() {
-    return Container(
-      height: 10,
-      decoration: BoxDecoration(
-          border: Border(top: BorderSide(width: 2.0, color: Colors.red))),
-      child: SizedBox(height: 8),
-    );
-  }
-
-  Widget _imageView() {
-    MediaQueryData queryData;
-    queryData = MediaQuery.of(context);
-
-    final image = widget._viewModel.loader.loadCardImage(
-        widget._viewModel.movie.poster, queryData.size.width * 0.7);
-
-    return Center(
-        child: ClipRRect(borderRadius: BorderRadius.circular(4), child: image));
-  }
-
-  Widget _titleFavoriteRow() {
-    return Row(
-      children: [_cardMovieTitle(), _favoriteButton()],
-    );
-  }
-
-  Widget _cardMovieTitle() {
-    return Flexible(
-        fit: FlexFit.tight,
-        child: Text(widget._viewModel.movie.title,
-            maxLines: 2,
-            textAlign: TextAlign.left,
-            style: TextStyle(fontSize: 18)));
-  }
-
-  Widget _favoriteButton() {
-    return Flexible(
-        fit: FlexFit.loose,
-        child: Card(
-            child: IconButton(
-          icon: Icon(CupertinoIcons.heart_fill,
-              color: isFavorited ? Colors.red : Colors.black54),
-          onPressed: () {
-            widget._viewModel.handleFavoriteSelection();
-            setState(
-              () {
-                isFavorited = !isFavorited;
-              },
-            );
-          },
-        )));
-  }
-
-  Widget _releaseDate() {
-    return Container(
-        decoration: BoxDecoration(
-            border: Border(top: BorderSide(width: 2.0, color: Colors.purple))),
-        padding: EdgeInsets.only(top: 8),
-        child: Text(widget._viewModel.movie.releaseDate,
-            maxLines: 10, style: TextStyle(fontSize: 18)));
-  }
-
-  Widget _overview() {
-    return Container(
-        decoration: BoxDecoration(
-            border: Border(top: BorderSide(width: 2.0, color: Colors.purple))),
-        padding: EdgeInsets.only(top: 8),
-        child: Text(widget._viewModel.movie.overview,
-            maxLines: 10, style: TextStyle(fontSize: 18)));
   }
 }
