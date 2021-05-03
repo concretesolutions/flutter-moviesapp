364d7a9ab79e40b197beed810f969614707e0678
cs-gabriela-lima <gabriela.s.lima@concrete.com.br>
2021-05-03 15:34:57 -0300

    Adds a native android detail view

--- android/app/src/main/kotlin/com/example/moviesapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/moviesapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.moviesapp
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.moviesapp.databinding.ActivityMainBinding
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
 
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+
+
         flutterEngine
                 .platformViewsController
                 .registry
