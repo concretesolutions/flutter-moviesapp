3ea0c1e5bd1b614077b24efa07fc1aa8a1750e4b
cs-gabriela-lima <gabriela.s.lima@concrete.com.br>
2021-05-10 10:13:13 -0300

    Adds Android communication

--- android/app/src/main/kotlin/com/example/moviesapp/NativeView/NativeView.kt
+++ android/app/src/main/kotlin/com/example/moviesapp/NativeView/NativeView.kt
@@ -3,16 +3,35 @@ package com.example.moviesapp.NativeView
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.moviesapp.R
 import io.flutter.plugin.platform.PlatformView
 
-class NativeView(context: Context) : PlatformView {
+class NativeView(context: Context, args: Any?) : PlatformView{
+
+    private val creationParams = args as HashMap<String, Any>?
+    private val baseUrl = "https://image.tmdb.org/t/p/w154/"
     private var inflater = LayoutInflater.from(context)
+    private val view = inflater.inflate(R.layout.activity_main, null)
 
     override fun getView(): View {
-        return inflater.inflate(R.layout.activity_main, null)
+        val movieTitle = view.findViewById<TextView>(R.id.movie_name)
+        val movieDescription = view.findViewById<TextView>(R.id.movie_description)
+        val movieReleaseDate = view.findViewById<TextView>(R.id.movie_release_date)
+        val moviePoster = view.findViewById<ImageView>(R.id.movie_image)
+        val url = baseUrl+creationParams?.get("poster_path")
+
+        movieTitle.text = creationParams?.get("title") as CharSequence?
+        movieDescription.text = creationParams?.get("overview") as CharSequence?
+        movieReleaseDate.text = creationParams?.get("release_date") as CharSequence?
+        Glide.with(moviePoster.context)
+                .load(url)
+                .into(moviePoster)
+        return view
     }
 
-    override fun dispose() {}
+    override fun dispose() {
+    }
 }
-
--- android/app/src/main/kotlin/com/example/moviesapp/NativeView/NativeViewFactory.kt
+++ android/app/src/main/kotlin/com/example/moviesapp/NativeView/NativeViewFactory.kt
@@ -1,12 +1,16 @@
 package com.example.moviesapp.NativeView
 
 import android.content.Context
+import com.example.moviesapp.R
+import io.flutter.plugin.common.JSONMessageCodec
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class NativeViewFactory: PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+import kotlin.collections.HashMap
+
+class NativeViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
     override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
-        return NativeView(context)
+        return NativeView(context, args)
     }
 }
