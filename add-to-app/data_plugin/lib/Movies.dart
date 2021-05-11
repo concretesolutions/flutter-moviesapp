0a72116ecbaf7dd31b8040e0ead721f4d73d121e
cs-mateus-santos <mateus.santos@concrete.com.br>
2021-05-10 23:43:01 -0300

    Fix problem in cast

---
+++ add-to-app/data_plugin/lib/Movies.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'dart:ffi';
+
+import 'package:data_plugin/Movie.dart';
+
+class MovieReturn {
+  List listMovies = [];
+
+  MovieReturn(this.listMovies);
+}
--- add-to-app/data_plugin/lib/data_plugin.dart
+++ add-to-app/data_plugin/lib/data_plugin.dart
@@ -1,17 +1,27 @@
 import 'dart:async';
 
+import 'package:data_plugin/Movie.dart';
+import 'package:data_plugin/Movies.dart';
 import 'package:flutter/services.dart';
 
 class DataPlugin {
   static const MethodChannel _channel = const MethodChannel('data_plugin');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<MovieReturn?> get getMovies async {
+    final moviesResult = await _channel.invokeMethod('dataRealmSwiftFetch');
+    var list = [];
+    var nativeResponse = List.castFrom(moviesResult);
+
+    nativeResponse.forEach((objeto) {
+      var movie = objeto as Movie;
+      list.add(movie);
+    });
+
+    return MovieReturn(list);
   }
 
-  static Future<String?> get getMovies async {
-    final String? version = await _channel.invokeMethod('dataRealmSwiftFetch');
-    return version;
+  static Future<bool?> get saveMovie async {
+    final result = await _channel.invokeMethod('dataRealmSwiftInsert');
+    return true;
   }
 }
